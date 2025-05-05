8133840a18e2ef1fa42749fcc56a80c4c408c49a
kevel choumene <k.choumene@cloudfacile.com>
2025-05-05 13:17:22 +0100

    feat(Add cronjob): cron de generation des facture auto ref31731

--- invoice/models.py
+++ invoice/models.py
@@ -32,7 +32,7 @@ class GenericInvoice(GenericInvoiceQuerysetMixin, HistoryBusinessModel):
     thirdparty = GenericForeignKey('thirdparty_type', 'thirdparty_id')
 
     code_tp = models.CharField(db_column='CodeTp', max_length=255, blank=True, null=True)
-    code = models.CharField(db_column='Code', max_length=255, null=False)
+    code = models.CharField(db_column='Code', max_length=255, null=False, unique=True)
     code_ext = models.CharField(db_column='CodeExt', max_length=255, blank=True, null=True)
 
     date_due = DateField(db_column='DateDue', blank=True, null=True)
@@ -68,7 +68,7 @@ class GenericInvoice(GenericInvoiceQuerysetMixin, HistoryBusinessModel):
 
 
 class GenericInvoiceLineItem(GenericInvoiceQuerysetMixin, HistoryBusinessModel):
-    code = models.CharField(db_column='Code', max_length=255, null=False)
+    code = models.CharField(db_column='Code', max_length=255, null=False, unique=True)
 
     description = models.TextField(db_column='Description', blank=True, null=True)
     details = models.JSONField(db_column='Details', blank=True, null=True)
