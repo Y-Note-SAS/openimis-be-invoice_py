43d32568880a5159c6513fccac0836f2799c07e8
kevel choumene <k.choumene@cloudfacile.com>
2026-02-09 09:22:18 +0100

    feat: change cron time

--- invoice/scheduled_tasks.py
+++ invoice/scheduled_tasks.py
@@ -759,7 +759,7 @@ def schedule_tasks(scheduler: BackgroundScheduler):
     """
     scheduler.add_job(
         invoice_generation_job,
-        trigger=CronTrigger(day='4,9,14,19', hour=3, minute=0),
+        trigger=CronTrigger(day='4,9,14,19', hour=9, minute=35),
         id="automatic_invoices_generation",
         max_instances=1,
         replace_existing=True,
