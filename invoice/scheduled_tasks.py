e342e64a13f9b0ae3d2d7f7defb655dcbd54cc7d
kevel choumene <k.choumene@cloudfacile.com>
2026-03-13 11:29:23 +0100

    feat: set invoice generation on the evening ref35884

--- invoice/scheduled_tasks.py
+++ invoice/scheduled_tasks.py
@@ -766,7 +766,7 @@ def schedule_tasks(scheduler: BackgroundScheduler):
     """
     scheduler.add_job(
         invoice_generation_job,
-        trigger=CronTrigger(day='4,9,14,19', hour=3, minute=0),
+        trigger=CronTrigger(day='4,9,14,19', hour=21, minute=0),
         id="automatic_invoices_generation",
         max_instances=1,
         replace_existing=True,
