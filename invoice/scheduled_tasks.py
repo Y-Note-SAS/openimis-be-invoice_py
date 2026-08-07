520bfd9db955f4ca07c614edc3b07370afbda6ad
kevel choumene <k.choumene@cloudfacile.com>
2026-08-07 13:17:06 +0100

    feat: réajustement thirdparty id

--- invoice/scheduled_tasks.py
+++ invoice/scheduled_tasks.py
@@ -17,6 +17,7 @@ from invoice.services import InvoiceService
 from invoice.services.invoiceLineItem import InvoiceLineItemService
 from invoice.apps import InvoiceConfig
 from django.db.models import Q
+from policyholder.models import PolicyHolder
 
 logger = logging.getLogger(__name__)
 
@@ -657,7 +658,15 @@ def invoice_generation_job():
                                                     logger.warning("head insuree %s ",
                                                         policy.family.head_insuree)
                                                     # create goverment invoice
-                                                    if government_amount > 0:
+                                                    policy_holder = PolicyHolder.objects.filter(
+                                                        is_deleted=False,
+                                                        code="AFD"
+                                                    ).filter(
+                                                        Q(date_valid_to__isnull=True) |
+                                                        Q(date_valid_to__date__gte=today.date())
+                                                    ).first()
+                                                    logger.warning("policy holder found %s", policy_holder)
+                                                    if government_amount > 0 and policy_holder:
                                                         values = {
                                                             "code": code,
                                                             "date_due": date_due,
@@ -671,8 +680,8 @@ def invoice_generation_job():
                                                         if policy.family.head_insuree:
                                                             values["subject_id"] = family.head_insuree.id
                                                             values["subject_type"] = "insuree"
-                                                            values["thirdparty_id"] = family.head_insuree.id
-                                                            values["thirdparty_type"] = "insuree"
+                                                            values["thirdparty_id"] = policy_holder.id
+                                                            values["thirdparty_type"] = "policyholder"
                                                             if family_amount > 0:
                                                                 # update code as two invoice will be
                                                                 # created as the code is unique
@@ -766,7 +775,7 @@ def schedule_tasks(scheduler: BackgroundScheduler):
     """
     scheduler.add_job(
         invoice_generation_job,
-        trigger=CronTrigger(day='4,9,14,19', hour=3, minute=0),
+        trigger=CronTrigger(day='4,9,14,19', hour=21, minute=0),
         id="automatic_invoices_generation",
         max_instances=1,
         replace_existing=True,
