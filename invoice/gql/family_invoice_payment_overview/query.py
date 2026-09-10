3af4068999d4164d89dfe604dac22eaf68edc3eb
Maxime NGOE <maxime.ngoe@gmail.com>
2026-09-10 22:35:46 +0100

    Update query.py

--- invoice/gql/family_invoice_payment_overview/query.py
+++ invoice/gql/family_invoice_payment_overview/query.py
@@ -7,7 +7,6 @@ from collections import defaultdict
 from datetime import date
 from decimal import Decimal
 from uuid import UUID
-
 import graphene
 from django.conf import settings
 from django.contrib.auth.models import AnonymousUser
