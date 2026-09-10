f37b23cd9af279669bf2b69c1dc5c6bad5fc109f
Maxime NGOE <maxime.ngoe@gmail.com>
2026-09-10 22:41:55 +0100

    Update query.py

--- invoice/gql/family_invoice_payment_overview/query.py
+++ invoice/gql/family_invoice_payment_overview/query.py
@@ -12,7 +12,6 @@ from django.conf import settings
 from django.contrib.auth.models import AnonymousUser
 from django.contrib.contenttypes.models import ContentType
 from django.core.cache import cache
-
 from invoice.apps import InvoiceConfig
 from invoice.models import DetailPaymentInvoice, Invoice
 from insuree.models import Insuree
