19e5b5a04da72fe2e4941744dbddb9ea9ebad9e3
kevel choumene <k.choumene@cloudfacile.com>
2026-09-14 16:03:44 +0100

    feat add fields for mutation ref37884

--- invoice/gql/payment_invoice/mutation.py
+++ invoice/gql/payment_invoice/mutation.py
@@ -21,6 +21,7 @@ from invoice.models import (
     PaymentInvoiceMutation,
     DetailPaymentInvoice
 )
+from ledger.models import AnalyticAxis
 
 
 class CreatePaymentInvoiceMutation(BaseHistoryModelCreateMutationMixin, BaseMutation):
@@ -65,6 +66,8 @@ class CreatePaymentInvoiceWithDetailMutation(BaseHistoryModelCreateMutationMixin
             data.pop('client_mutation_id')
         if "client_mutation_label" in data:
             data.pop('client_mutation_label')
+        party_type = ContentType.objects.get_for_model(AnalyticAxis)
+        data["party_type"] = party_type
         status, subject_id, subject_type = cls._get_field_for_detail(data)
         payment_invoice = cls.create_object(user=user, object_data=data)
         if payment_invoice:
